0e54622178fa51bf02b7e3bce26df0ab8647c387
pachara-jinnikorn <122819026+pachara-jinnikorn@users.noreply.github.com>
2023-09-11 12:19:00 +0000

    1

--- src/main/java/th/mfu/Concert.java
+++ src/main/java/th/mfu/Concert.java
@@ -5,22 +5,52 @@ import java.util.Date;
 public class Concert {
 
     //TODO: add attributes
+    private int id;
+    private String title;
+    private String performer;
+    private Date date;
+    private String description;
 
-    public Concert() {
-    }
+    public Concert() {}
+
     public Concert(String title, String description) {
         //TODO: set attributes
+        this.title = title;
+        this.description = description;
     }
  
+    //TODO: add getters and setters
+    public int getId() {
+        return id;
+    }
     public String getTitle() {
         //TODO: return title
-        return "";
+        return title;
+    }
+    public String getPerformer() {
+        return performer;
+    }
+    public Date getDate() {
+        return date;
     }
     public String getDescription() {
         //TODO: return description
-        return "";
+        return description;
     }
-
-    //TODO: add getters and setters
-
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setPerformer(String performer) {
+        this.performer = performer;
+    }
+    public void setDate(Date date) { // change to specific date
+        this.date = date;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
--- src/main/java/th/mfu/ConcertController.java
+++ src/main/java/th/mfu/ConcertController.java
@@ -20,44 +20,61 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class ConcertController {
     // TODO: create hashmap of concerts for storing data
-
-    //TODO: add initbinder to convert date
+    private HashMap<Integer, Concert> concertMap = new HashMap<>();
+    private int nextId = 1;
+    
+    // TODO: add initbinder to convert date
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm");
+        dateFormat.setLenient(false);
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, false));
+    }
 
     @GetMapping("/concerts")
     public String listConcerts(Model model) {
         // TODO: add concerts to model
+        model.addAttribute("concerts", concertMap.values());
         // TODO: return a template to list concerts
-        return "";
+        return "list-concert";
     }
 
     @GetMapping("/add-concert")
     public String addAConcertForm(Model model) {
         // TODO: pass blank concert to a form
+        model.addAttribute("concert", new Concert());
         // TODO: return a template for concert form
-        return "";
+        return "add-concert-form"; // Return the template name
     }
 
     @PostMapping("/concerts")
     public String saveConcert(@ModelAttribute Concert concert) {
         // TODO: add concert to list of concerts
         // TODO: increment nextId
+        int id = nextId++; // (unique-id) Get the next available ID and increment it
+        concert.setId(id);
+        concertMap.put(id, concert); // Add the concert to the map with the new ID
         // TODO: redirect to list concerts
-        return "";
+        return "redirect:/concerts"; // Redirect to the list of concerts
     }
 
     @GetMapping("/delete-concert/{id}")
     public String deleteConcert(@PathVariable int id) {
         // TODO: remove concert from list of concerts
+        Concert inst = concertMap.getOrDefault(id, null);
+        if (inst != null) {
+            concertMap.remove(id);
+        }
         // TODO: redirect to list concerts
-        return "";
+        return "redirect:/concerts"; // Redirect to the list of concerts
     }
 
-    
     @GetMapping("/delete-concert")
     public String removeAllConcerts() {
         //TODO: clear all employees and reset id
+        concertMap.clear();
+        nextId = 1;
         // TODO: redirect to list concerts
-        return "";
+        return "redirect:/concerts";
     }
-
 }
